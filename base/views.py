c24d66d5c2e10d1ae2d9d373130938bea2c2f3a8
Felipe Díaz <andresdiaz9822@gmail.com>
2021-05-10 14:04:25 -0500

    Delete Item
    Class based view for deleting tasks.

--- base/views.py
+++ base/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView 
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
 
 class TaskList(ListView):
     model = Task
-    context_object_name = "tasks"
+    context_object_name = 'tasks'
 
 class TaskDetail(DetailView):
     model = Task
-    context_object_name = "task"
-    template_name = "base/task.html"
+    context_object_name = 'task'
+    template_name = 'base/task.html'
 
 class TaskCreate(CreateView):
     model = Task
@@ -23,4 +23,10 @@ class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+    
 
